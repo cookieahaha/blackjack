72aa4dc7cd77c3b1f15118c6f60551e4da54eab6
cookieahaha <programmer@kashii-no-MacBook-Pro-9.local>
2016-01-24 16:32:47 -0800

    hmmm
    hmmm

--- src/main/java/me/kuk/game/card/blackjack/Html.java
+++ src/main/java/me/kuk/game/card/blackjack/Html.java
@@ -25,48 +25,49 @@ public class Html{
 		p.println("<p>test</p>");
 		//new game
 		if(this.gameStarted == false){
-			p.println("<form action=\"blackjack\">");
+			p.println("<form action=\"bj\">");
 			p.println("<input type=\"hidden\" name=\"a\" value=\"start the game\">");
 			p.println("<input type=\"submit\" value=\"Start the game\">");
 			p.println("</form>");
+			this.gameStarted = true;
 		}
 		else{
+			p.println("<p>iojfrmcniunv</p>");			
+
 			//bet
-			p.println("<form action=\"blackjack\">");
+			p.println("<form action=\"bj\">");
 			p.println("<input type=\"hidden\" name=\"a\" value=\"bet\">");
 			p.println("<input type=\"submit\" value=\"bet\">");
 			p.println("</form>");
 
-		if(this.gameStarted){
-		p.println("wins"); //win
-		p.println("losses"); //loss
+			p.println("wins"); //win
+			p.println("losses"); //loss
 
-		p.println("You");   //player name
-		p.println("");   //player cards
-		p.println("");  //player score
-		p.println("vs");  //vs
-		p.println("Dealer");   //dealer name
-		p.println("");   //dealer cards
-		p.println("");  //dealer score
+			p.println("You");   //player name
+			p.println("");   //player cards
+			p.println("");  //player score
+			p.println("vs");  //vs
+			p.println("Dealer");   //dealer name
+			p.println("");   //dealer cards
+			p.println("");  //dealer score
 
-		//hit
-		p.println("<form action=\"blackjack\">");
-		p.println("<input type =\"hidden\" name=\"a\" value=\"hit\">");
-		p.println("<input type=\"submit\" value=\"hit\">");
-		p.println("</form>");
+			//hit
+			p.println("<form action=\"bj\">");
+			p.println("<input type =\"hidden\" name=\"a\" value=\"hit\">");
+			p.println("<input type=\"submit\" value=\"hit\">");
+			p.println("</form>");
 		
-		//stand
-		p.println("<form action=\"blackjack\">");
-		p.println("<input type = \"hidden\" name =\"a\" value =\"stand\">");
-		p.println("<input type=\"submit\" value=\"stand\">");
-		p.println("</form>");
+			//stand
+			p.println("<form action=\"bj\">");
+			p.println("<input type = \"hidden\" name =\"a\" value =\"stand\">");
+			p.println("<input type=\"submit\" value=\"stand\">");
+			p.println("</form>");
 
-		//double
-		p.println("<form action=\"blackjack\">");
-		p.println("<input type =\"hidden\" name =\"a\" value=\"double\">");
-		p.println("<input type=\"submit\" value=\"double\">");
-		p.println("</form>");
-		}
+			//double
+			p.println("<form action=\"bj\">");
+			p.println("<input type =\"hidden\" name =\"a\" value=\"double\">");
+			p.println("<input type=\"submit\" value=\"double\">");
+			p.println("</form>");
 		}
 
 		p.println("</body>");
