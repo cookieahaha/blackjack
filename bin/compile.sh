d71d44e530871d126f28c755d91269316791f8e8
cookieahaha <programmer@kashii-no-MacBook-Pro-9.local>
2016-01-30 15:21:47 -0800

    compile ok
    ok

--- bin/compile.sh
+++ bin/compile.sh
@@ -3,6 +3,7 @@
 mkdir -p target/classes
 javac -d target/classes -classpath src/main/java/me/kuk/game/card/blackjack:lib/servlet-api-3.1.jar src/main/java/me/kuk/game/card/blackjack/*.java
 mv src/main/java/me/kuk/card/blackjack/*.class target/classes
-jar cvf target/Blackjack.war target/WEB-INF
-cp target/Blackjack.war $HOME/jetty-distribution-9.3.6.v20151106/webapps
+cd target
+jar cvf Blackjack.war WEB-INF
+cp Blackjack.war $HOME/jetty-distribution-9.3.6.v20151106/webapps
 
